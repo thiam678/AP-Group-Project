cb25a1e7020807700c4607c980cac18bf4ad4cfa
AStewart21 <93847247+AStewart21@users.noreply.github.com>
2022-10-23 13:54:49 -0500

    Update CustomerForm.java

--- view/CustomerForm.java
+++ view/CustomerForm.java
@@ -21,25 +21,10 @@ public class CustomerForm {
     private JLabel idLabel;
     private JLabel nameLabel;
 
-    private JLabel dobLabel;
-
-    private JLabel addressLabel;
-
-    private JLabel telephoneLabel;
-
-    private JLabel emailLabel;
-
-    private JLabel domLabel;
-
-    private JLabel domexLabel;
+    
     private JTextField idTextField;
     private JTextField nameTextField;
-    private JTextField dobTextField;
-    private JTextField addressTextField;
-    private JTextField telephoneTextField;
-    private JTextField emailTextField;
-    private JTextField domTextField;
-    private JTextField domexTextField;
+    
     private JButton save;
     private GridBagConstraints gbc;
 
@@ -54,21 +39,7 @@ public class CustomerForm {
         nameLabel = new JLabel("Name");
         nameTextField = new JTextField();
 
-        dobLabel =new JLabel("Date of Birth");
-        dobTextField =new JTextField();
-
-        addressLabel =new JLabel("Address");
-        addressTextField =new JTextField();
-
-        telephoneLabel =new JLabel("Telephone Number");
-        telephoneTextField =new JTextField();
-
-        emailLabel =new JLabel("Email");
-        emailTextField =new JTextField();
-
-        domLabel =new JLabel("Date of Membership");
-        domTextField =new JTextField();
-
+      
 
         save= new JButton("Save");
 
@@ -97,71 +68,7 @@ public class CustomerForm {
         gbc.ipadx = 150;
         frame.add(nameTextField, gbc);
 
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.insets = new Insets(10, 0, 0, 0);
-        frame.add(dobLabel, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.insets = new Insets(10, 30, 0, 0);
-        gbc.ipadx = 150;
-        frame.add(dobTextField, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.insets = new Insets(10, 0, 0, 0);
-        frame.add(addressLabel, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        gbc.insets = new Insets(10, 30, 0, 0);
-        gbc.ipadx = 150;
-        frame.add(addressTextField, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.insets = new Insets(10, 0, 0, 0);
-        frame.add(telephoneLabel, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 4;
-        gbc.insets = new Insets(10, 30, 0, 0);
-        gbc.ipadx = 150;
-        frame.add(telephoneTextField, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        gbc.insets = new Insets(10, 0, 0, 0);
-        frame.add(emailLabel, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 5;
-        gbc.insets = new Insets(10, 30, 0, 0);
-        gbc.ipadx = 150;
-        frame.add(emailTextField, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 6;
-        gbc.insets = new Insets(10, 0, 0, 0);
-        frame.add(domLabel, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 6;
-        gbc.insets = new Insets(10, 30, 0, 0);
-        gbc.ipadx = 150;
-        frame.add(domTextField, gbc);
-
+        
 
 
         gbc = new GridBagConstraints();
