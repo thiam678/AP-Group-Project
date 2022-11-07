ae72ccd048dc3088dadcf77f4feb914e8e234d6e
ojayp <114765303+ojayp@users.noreply.github.com>
2022-11-07 12:42:40 -0600

    Update StaffController.java

--- controller/StaffController.java
+++ controller/StaffController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,12 +17,13 @@ public class StaffController {
     private static int affectedRows = 0;
 
     public void createEmployeeRecord(Employee obj) {
-        String query = "INSERT INTO janswholesale.staff (empID, name, empEmail, empTele)"
-                + "VALUES ('"+ obj.getEmpID() + "' , '"+ obj.getName() +"', '" + obj.getEmpEmail()+"','"+obj.getEmpTele()+"')'";
+        String query = "INSERT INTO employee.employee (empID, name, empEmail, empTele)"
+                + "VALUES ('"+ obj.getEmpID() + "' , '"+ obj.getName() +"', '" + obj.getEmpEmail()+"','"+obj.getEmpTele()+"',)'";
         try {
             stmt = dbConn.createStatement();
             affectedRows = stmt.executeUpdate(query);
-            if(affectedRows == 1) {
+            
+            if(affectedRows ==1) {
                 JOptionPane.showMessageDialog(null,
                         "Record Created", "Record creation status",
                         JOptionPane.INFORMATION_MESSAGE);
@@ -31,6 +33,25 @@ public class StaffController {
         }catch(Exception e) {
             System.err.println("ERROR Creating Record: " +e.getMessage());
         }
+    }
+    public void viewEmployeeRecord(Employee obj) throws SQLException {
+    	
+    	 Statement statement = StaffController.dbConn.createStatement();
+    	    String view = "select * from employee";
+    	    ResultSet resultSet = statement.executeQuery(view);
+    	    
+    	    try {
+                statement = dbConn.createStatement();
+                while (resultSet.next()) {
+                
+                	JOptionPane.showMessageDialog(null, view);
+                }
+                	
+    	    }catch(Exception e) {
+                System.err.println("Record " +e.getMessage());
+            }
+    	    
+
     }
 
 }
