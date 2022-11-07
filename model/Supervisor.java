cfb7193a12c706432ad9585e7203bf978af0a433
ojayp <114765303+ojayp@users.noreply.github.com>
2022-11-07 13:13:23 -0600

    Add files via upload

---
+++ model/Supervisor.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Supervisor extends Employee{
+	
+	Department Dep;
+	
+	public Supervisor() {
+		super();
+		Dep = new Department();
+	}
+	
+	public Supervisor(String EmpId, String name, String EmpTele, String EmpEmail, Department Depart )
+	{
+		super(EmpEmail,EmpTele,name,EmpEmail, Depart);
+		this.Dep = Depart ;
+	
+	}
+
+	public Department getDep() {
+		return Dep;
+	}
+
+	public void setDep(Department dep) {
+		Dep = dep;
+	}
+	
+	public String Tostring() {
+		return "employee ID ="  +getEmpID() + "\n"+ "Name = "
+	+ getName() + "\n"+ "Employee Email = " + getEmpEmail() +"\n"+
+				"Employee Telephone = " + getEmpTele() +"/n" +"Department ="  +Dep+ "" +   "/n" ;
+	}
+
+}
